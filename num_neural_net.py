1be81045020c8cfdcc0961495148ac07efb408a7
SSP-ANO <alexei.ozerov.7@gmail.com>
2017-12-08 23:13:00 -0500

    update

--- num_neural_net.py
+++ num_neural_net.py
@@ -1,33 +1,88 @@
 # Number Recognition Neural Network
 
-# Anaconda3-5.0.0
+# written using Anaconda3-5.0.0
 
 import numpy 
+import scipy.special
 
 # neural network class definition
 class neuralNetwork:
 
 	# initialise the neural network
-	def _init_(self, inputnodes, hiddennodes, outputnotes, learningrate):
+	def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
 		
 		# set the number of nodes in each input, hidden, output layer
-		
 		self.inodes = inputnodes
 		self.hnodes = hiddennodes
 		self.onodes = outputnodes
 		
-		# learning rate
+		# link weight matrices Wih Who
+		# weights inside the arrays are w_i_j, withere link is from node i to node j in the next layer
+		# w11 w21
+		# w12 w22 etc
+		self.wih = numpy.random.normal(0.0,pow(self.hnodes, -0.5), (self.hnodes,self.inodes))
+		self.who = numpy.random.normal(0.0,pow(self.onodes, -0.5), (self.onodes,self.hnodes))
 		
-		self.lr = learningrate
+		# learning rate
+		self.lr = learningrate	
+
+		# activation function is the sigmoid function
+		self.activation_function = lambda x: scipy.special.expit(x)
+
 		pass
 
 	# train the neural network
-	def train():
-		pass
+	def train(self, inputs_list, targets_list):
+
+            # convert inputs list to 2d array
+            inputs = numpy.array(inputs_list, ndmin=2).T
+            targets = numpy.array(targets_list, ndmin=2).T
+
+            # calculate signals into hidden layer
+            hidden_inputs = numpy.dot(self.wih, inputs)
+
+            # calculate the signals emerging from hidden layer
+	    hidden_outputs = self.activation_function(hidden_inputs)
+
+            # calculate signals into final output layer
+            final_inputs = numpy.dot(self.who, hidden_outputs)
+
+            # calculate the signals emerging from final output layer
+            final_outputs = self.activation_function(final_inputs)
+
+            # output layer error is the (target - actual)
+            output_errors = targets - final_outputs
+
+            # hidden layer error is the output_errors, split by weights, recombined at hidden nodes
+            hidden_errors = numpy.dot(self.who.T, output_errors)
+
+            # update the weights for the links between the hidden and output layers
+            self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(hidden_outputs))
+
+            # update the weights for the links between the input and hidden layers
+            self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
+
+            pass
 
 	# query the neural network
-	def query():
-		pass
+	def query(self, inputs_list):
+		
+		# converts input list to 2d array
+                inputs = numpy.array(inputs_list, ndmin=2).T
+
+                # calculate signals into hidden layer
+                hidden_inputs = numpy.dot(self.wih, inputs)
+
+                # calculate the signals emerging from hidden layer
+                hidden_outputs = self.activation_function(hidden_inputs)
+
+                # calculate signals into final output layer
+                final_inputs = numpy.dot(self.who, hidden_outputs)
+
+                # calculate the signals emerging from final output layer
+                final_outputs = self.activation_function(final_inputs)
+
+                return final_outputs
 
 # test network configuration
 input_nodes = 3
@@ -36,15 +91,7 @@ output_nodes = 3
 learning_rate = 0.3
 
 # creating an instance
+n = neuralNetwork(input_nodes,hidden_nodes,output_nodes, learning_rate)
 
-n = neuralNetwork(input_nodes,hidden_nodes,output_nodes,learning_rate)
-
-# link weight matrices Wih Who
-# weights inside the arrays are w_i_j, withere link is from node i to node j in the next layer
-# w11 w21
-# w12 w22 etc
-
-self.wih = numpy.random.normal(0.0,pow(self.hnodes, -0.5), (self.hnodes,self.inodes))
-self.who = numpy.random.normal(0.0,pow(self.onodes, -0.5), (self.onodes,self.hnodes))
-
-
+# test query (doesn't mean anything useful yet)
+n.query([1.0, 0.5, -1.5])
